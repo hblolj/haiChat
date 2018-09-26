9a4c9ebe74696596f72b97ba57e09ab72f23e795
hblolj <hblolj@gmail.com>
2018-09-26 09:46:46 +0800

    后端服务初始化建立
    1. 添加数据库配置
    2. 添加访问路径设置

---
+++ src/main/java/com/hblolj/haichat/HaichatApplication.java
@@ -0,0 +1,12 @@
+package com.hblolj.haichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaichatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hblolj/haichat/HaichatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hblolj.haichat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HaichatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
